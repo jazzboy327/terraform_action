d28faa300a0aa5eabfc1ef69b13eac46f2d0d0cf
sooyoun.no <>
2025-07-16 22:39:38 +0900

    script fix

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 set -e
 
 echo "[1] 이슈 텍스트 파싱 → terraform.tfvars 생성"
-bash scripts/parse-issue-to-tfvars.sh local_test.txt > environments/dev/tfvars/terraform.tfvars
+bash scripts/parse-issue-to-tfvars.sh local_test.txt > environments/dev/terraform.tfvars
 
 echo "[2] 민감 정보 추가"
-echo 'admin_password = "azureuser1234!@#$"' >> environments/dev/tfvars/terraform.tfvars
+echo 'admin_password = "azureuser1234!@#$"' >> environments/dev/terraform.tfvars
 
 echo "[3] Terraform 실행"   
 cd environments/dev
 terraform init
-terraform plan -var-file=tfvars/terraform.tfvars -out=tfplan
+terraform plan -out=tfplan
 terraform apply -auto-approve tfplan
 cd ../../
