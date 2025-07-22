2fafab30494d0258aeb76a3aa9cd688890338ba3
jazzboy327 <108048281+jazzboy327@users.noreply.github.com>
2025-07-22 07:20:43 +0000

    feat: add terraform config for teams/dev6

---
+++ teams/dev6/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-backend-rg"
+    storage_account_name = "axdtfstatestorageac" # only lowcase!!  
+    container_name       = "tfstate"
+    key                  = "teams/dev6/terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
