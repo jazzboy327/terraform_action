97ed6ef5c311095b97c0226958f7c6da23765490
sooyoun.no <>
2025-07-22 15:43:13 +0900

    step fix

--- scripts/create_team_tf.sh
+++ scripts/create_team_tf.sh
@@ -3,13 +3,13 @@
 TEAM=$1
 
 
-cat > "./teams/${TEAM}/backend.tf" <<EOF
+cat > "${TEAM}/backend.tf" <<EOF
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-backend-rg"
     storage_account_name = "axdtfstatestorageac" # only lowcase!!  
     container_name       = "tfstate"
-    key                  = "teams/${TEAM}/terraform.tfstate"
+    key                  = "${TEAM}/terraform.tfstate"
   }
   required_providers {
     azurerm = {
