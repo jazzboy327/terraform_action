5c7b5b0ea4d5335bfcd4f0bbbf19cfb896cb2bba
sooyoun.no <>
2025-07-22 17:05:52 +0900

    module fix

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,34 +1,5 @@
-provider "azurerm" {
-  features {}
-}
-
-locals {
-  common_tags = {
-    Environment = var.environment
-    Team        = var.team_name
-    Service     = var.service_name
-    Owner       = "kt"
-    Created     = timestamp()
-  }
-}
-
-module "rg" {
-  source   = "../../modules/resource_group"
-  name     = var.resource_group_name
-  location = var.location
-}
-
-module "vnet" {
-  source                  = "../../modules/network"
-  vnet_name               = var.vnet_name
-  location                = var.location
-  resource_group_name     = module.rg.name
-  address_space           = var.address_space
-  subnet_address_prefixes = ["10.0.1.0/24"]
-}
 
 module "vm" {
-  source              = "../../modules/vm"
   vm_name             = var.vm_name
   location            = var.location
   resource_group_name = module.rg.name
@@ -36,7 +7,7 @@ module "vm" {
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   subnet_id           = module.vnet.subnet_id
-  tags                = local.common_tags
+  tags                = var.tags
 }
 
 
