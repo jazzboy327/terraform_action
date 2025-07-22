81aba4e72b62cf4f2fd25a8385f74b9e6b3ad322
sooyoun.no <>
2025-07-23 00:30:03 +0900

    output fix

--- template/outputs.tf
+++ template/outputs.tf
@@ -1,19 +1,19 @@
 output "vm_id" {
   description = "The ID of the Virtual Machine"
-  value       = azurerm_linux_virtual_machine.vm.id
+  value       = module.vm.vm_id
 }
 
 output "vm_name" {
   description = "The name of the Virtual Machine"
-  value       = azurerm_linux_virtual_machine.vm.name
+  value       = module.vm.vm_name
 }
 
 output "vm_private_ip" {
   description = "The private IP address of the Virtual Machine"
-  value       = azurerm_network_interface.vm_nic.private_ip_address
+  value       = module.vm.vm_private_ip
 }
 
 output "vm_public_ip" {
   description = "The public IP address of the Virtual Machine"
-  value       = azurerm_linux_virtual_machine.vm.public_ip_address
+  value       = module.vm.vm_public_ip
 } 
