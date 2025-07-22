044e1d2eb4ed1db7c72464a52f86f1b11fd995cb
sooyoun.no <>
2025-07-23 00:20:13 +0900

    variables fix

--- teams/dev13/variables.tf
+++ teams/dev13/variables.tf
@@ -26,6 +26,31 @@ variable "subnet_id" {
   type = string
 }
 
+variable "environment" {
+  description = "환경(dev, stage, prod 등)"
+  type        = string
+}
+
+variable "team_name" {
+  description = "팀 이름"
+  type        = string
+}
+
+variable "service_name" {
+  description = "서비스 이름"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "VNet 이름"
+  type        = string
+}
+
+variable "address_space" {
+  description = "VNet CIDR"
+  type        = list(string)
+}
+
 variable "tags" {
   type        = map(string)
   description = "Common resource tags"
