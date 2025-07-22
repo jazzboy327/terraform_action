c710c5b451f26d9acb972fe4fdf9e4a67e42b499
jazzboy327 <108048281+jazzboy327@users.noreply.github.com>
2025-07-22 07:13:35 +0000

    feat: add terraform config for teams/dev6

---
+++ teams/dev6/variables.tf
@@ -0,0 +1,33 @@
+variable "vm_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Common resource tags"
+  default     = {}
+} 
