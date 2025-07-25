a069bfb20fa51705f0912d9fe58097a0e19ebc38
sooyoun.no <>
2025-07-25 14:32:26 +0900

    backend.tf test name fix

--- scripts/create_team_tf.sh
+++ scripts/create_team_tf.sh
@@ -7,7 +7,7 @@ cat > "${TEAM}/backend.tf" <<EOF
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-backend-rg"
-    storage_account_name = "axdtfstatestorageac" # only lowcase!!  
+    storage_account_name = "axdtfstatestorageac1" # only lowcase!! 계정별로 다르게 설정해야함
     container_name       = "tfstate"
     key                  = "${TEAM}/terraform.tfstate"
   }
