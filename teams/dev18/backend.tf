fce4d3c70180ed695b2de580e8de95b11d507a6d
jazzboy327 <108048281+jazzboy327@users.noreply.github.com>
2025-07-25 06:12:38 +0000

    feat: add terraform config for teams/dev18

---
+++ teams/dev18/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-backend-rg"
+    storage_account_name = "axdtfstatestorageac1" # only lowcase!! 계정별로 다르게 설정해야함
+    container_name       = "tfstate"
+    key                  = "teams/dev18/terraform.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
